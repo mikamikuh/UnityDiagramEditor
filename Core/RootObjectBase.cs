4bb7eb25e3404243da804bea3a58603c7798b0ae
mikamikuh <mkouhei0910@gmail.com>
2013-01-01 23:01:47 +0900

    Delete serializable object from RootObjectBase class.

--- Core/RootObjectBase.cs
+++ Core/RootObjectBase.cs
@@ -5,32 +5,15 @@ using System.Collections.Generic;
 /// データをすべて保持するクラス
 /// このクラスを継承したクラスでをGameObjectに割り当て、それを選択したときにエディタは動作する
 /// </summary>
-[System.Serializable]
 public abstract class RootObjectBase : MonoBehaviour, IRootObject
 {
-    /// <summary>
-    /// ダイアグラム上のすべての要素を管理するリスト
-    /// </summary>
-    public List<DiagramElementBase> elements;
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    public RootObjectBase()
-    {
-        elements = new List<DiagramElementBase>();
-    }
-
     /// <summary>
     /// 描画可能なオブジェクトのリストを返す
     /// </summary>
     /// <returns>
     /// 描画可能なオブジェクトのIList(IDiagramElement)
     /// </returns>
-    public IList<DiagramElementBase> GetDrawableObject ()
-    {
-        return elements;
-    }
+    public abstract IList<DiagramElementBase> GetDrawableObject ();
 
     /// <summary>
     /// 対象のマウス座標に存在するノードを返す
@@ -43,7 +26,7 @@ public abstract class RootObjectBase : MonoBehaviour, IRootObject
     /// </returns>
     public INodeElement GetNodeElement(Vector2 position)
     {
-        foreach (IDiagramElement e in elements)
+        foreach (IDiagramElement e in GetDrawableObject())
         {
             if (e is INodeElement)
             {
